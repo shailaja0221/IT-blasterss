12645dde556d3ab60f47a2e08acee7a02a618337
shailaja0221 <48052903+shailaja0221@users.noreply.github.com>
2019-03-09 19:03:57 +0530

    Create lar.c

---
+++ lar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+     int n1=5,n2=4,n3=6;
+    if(n1>=n2 && n1>=n3)
+    printf("n1 the largest number.",n1);
+    if(n2>=n1 && n2>=n3)
+    printf("n2 is the largest number.", n2);
+    if(n3>=n1 && n3>=n2)
+    printf("n3 is the largest number.", n3);
+    
+}
