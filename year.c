5053c79a6c848b180c494344bd57fe7d5ef02166
shailaja0221 <48052903+shailaja0221@users.noreply.github.com>
2019-03-09 19:09:20 +0530

    Create year.c

---
+++ year.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("enter the year");
+    scanf("%d",&a);
+    if(a%4==0)
+    printf("is a leap year");
+    else
+    printf("not leap");
+}
+    
