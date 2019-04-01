80da22418c96cd9660271e976e91a845efeacd7b
shailaja0221 <48052903+shailaja0221@users.noreply.github.com>
2019-04-01 21:10:50 +0530

    Create 28.c

---
+++ 28.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+	void main()
+	{
+	int a[10],j,m;
+	printf("enter the limimt");
+	scanf("%d",&m);
+	printf("enter the element");
+	for(j=0;i<m;j++)
+	{
+	scanf("%d",&a[j]);
+	}
+	printf("the elements with index");
+	for(j=0;j<m;j++)
+	{
+	printf("%d\t%d\n",j,a[j]);
+	}
+	}
