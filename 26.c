f98083e66aab36d74bc9c73dafc3c52bcca3ca67
shailaja0221 <48052903+shailaja0221@users.noreply.github.com>
2019-04-01 16:28:43 +0530

    Create 26.c

---
+++ 26.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int a[10],j,m,o,sum=0;
+    printf("enter the limit");
+    scanf("%d",&m);
+    for(j=0;j<m;j++)
+    {
+        scanf("%d",&a[j]);
+    }
+    for(j=0;j<m;j++)
+    {
+        sum=sum+a[ij];
+    }
+    o=sum/m;
+    printf("%d",o);
+    
+}
