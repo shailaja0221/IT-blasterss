346098ffd7edcc52cf3925aabfba5788cfbb53ea
shailaja0221 <48052903+shailaja0221@users.noreply.github.com>
2019-04-03 14:44:38 +0530

    Create 4b.c

---
+++ 4b.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+int count=0,j,k;
+char string[20];
+scanf("%s",string);
+k=strlen(string);
+for(j=0;j<=l;j++)
+{
+if(string[i]==' ')
+{
+count++;
+}
+}
+printf("%d",count+1);
+}
